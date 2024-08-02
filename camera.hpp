c5e31ee394fa3bc75ea6046f3402b005a2da55cf
carovas <yulia_schroth@yahoo.fr>
2024-08-02 19:32:14 +0200

    Basic movements

---
+++ camera.hpp
@@ -0,0 +1,65 @@
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+#include "wfc.hpp"
+
+#include <raylib.h>
+
+#include <math.h>
+
+class Cam {
+public:
+    Cam(): pos_x(grid_size/2.f), pos_y(grid_size/2.f), speed(45.f), angle_speed(5.f)
+    {
+
+    }  
+
+    Vector2 getpos() {
+        return {pos_x, pos_y};
+    }
+
+    Vector2 getdelta() {
+        return {delta_x, delta_y};
+    }
+
+    void update(float dt) {
+
+        if(IsKeyDown(KEY_W))
+        {
+            pos_x = pos_x + delta_x * dt * speed;
+            pos_y = pos_y + delta_y * dt * speed;
+        }
+        if(IsKeyDown(KEY_S))
+        {
+            pos_x = pos_x - delta_x * dt * speed;
+            pos_y = pos_y - delta_y * dt * speed;
+        }
+        if(IsKeyDown(KEY_D))
+        {
+            angle = angle + angle_speed * dt;
+            if(angle > 2.f*PI)
+                angle = angle - 2.f*PI;
+            delta_x = cos(angle) * 5.f;
+            delta_y = sin(angle) * 5.f;
+        }
+        if(IsKeyDown(KEY_A))
+        {	
+            angle = angle - angle_speed * dt;
+            if(angle < 0.f)
+                angle = angle + 2.f*PI;
+            delta_x = cos(angle) * 5.f;
+            delta_y = sin(angle) * 5.f;
+        }        
+    }
+
+private:
+    float pos_x;
+    float pos_y;
+    float delta_x;
+    float delta_y;
+    float angle; // angle of the player
+    float speed;
+		float angle_speed;
+};
+
+#endif
